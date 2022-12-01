f8aafdd440ce9d7d00414c008a96eb1f2d1a4de0
theLiuk23 <ldvcoding@gmail.com>
2022-12-01 19:15:32 +0100

    bug fix

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import threading
-from spotipy import Spotify, SpotifyException
 from configparser import RawConfigParser
 from datetime import datetime
-from requests import post
+from spotipy import Spotify
 from platform import system
+from requests import post
 import asyncio
 import os, sys
 
@@ -22,60 +21,79 @@ class MySpotify:
 
     def get_volume(self) -> float:
         if self.operating_system == "Linux":
+            volume = None
             # if pulseaudio has an audio instance called Spotify this line will save a list containing the spotify's volumes
-            volume = [self.pulse.volume_get_all_chans(key) for key in self.pulse.sink_input_list() if key.name == "Spotify"]
-            if not len(volume): return 1.0 # spotify desktop isn't opened
-            return round(volume[0], 2) # rounds float number by 2 digits after comma
+            for key in self.pulse.sink_input_list():
+                if key.name == "Spotify":
+                    volume = self.pulse.volume_get_all_chans(key)
+            if volume:
+                return round(volume[0], 2) # rounds float number by 2 digits after comma
+            return 1.0 # spotify desktop isn't opened
         
 
     def set_volume(self, volume:float):
         if self.operating_system == "Linux":
             for key in self.pulse.sink_input_list():
-                if key.name == "Spotify": self.pulse.volume_set_all_chans(key, volume)
+                if key.name == "Spotify":
+                    self.pulse.volume_set_all_chans(key, volume)
 
 
     def playing_advert(self) -> bool:
         self.track = self.spotify.current_user_playing_track()
         if self.track is None:
             return False
+        # print("\n".join([self.track["item"]["name"] for key in self.track["item"] if key == "name"]))
         return self.track["currently_playing_type"] == "ad"
 
 
     async def main(self):
-        print("hey")
+        print("main in spotify")
         while running:
-            await asyncio.sleep(1)
-            if self.playing_advert():
+            # checks if token expires while the script is running
+            if not await Login().verify_login():
+                start()
+
+            # ad is being played, volume has to be muted
+            if self.playing_advert() and self.get_volume() > 0.0:
+                # saves volume's value before muting it
                 self.old_volume = self.get_volume()
-                print(f"Muting audio: {self.old_volume}")
+                print(f"Muting audio:    {self.old_volume}")
                 self.set_volume(0.0)
-            elif self.get_volume() == 0.0:
+            # ad stopped being played, volume has to be restored
+            elif not self.playing_advert() and self.get_volume() == 0.0:
                 print(f"Restoring audio: {self.old_volume}")
                 self.set_volume(self.old_volume)
+
+            await asyncio.sleep(1)
 
 
 
 class Login:
-    def __init__(self, base_64:str, refresh_token:str, old_token:str):
+    def __init__(self):
         self.time_format = r"%Y-%m-%d %H:%M:%S.%f"
-        self.base_64 = base_64
-        self.refresh_token = refresh_token
-        self.old_token = old_token
+        self.base_64 = Settings().get_info_from_secrets("base_64")
+        self.refresh_token = Settings().get_info_from_secrets("refresh_token")
+        self.old_token = Settings().get_info_from_secrets("old_token")
+        self.last_time = Settings().get_info_from_data("last_time_token_refreshed")
     
         
     # it checks if the old token has expired (older than 1h)
-    # if it is, a new token will be generated
-    async def verify_login(self) -> str:
-        last_time = Settings().get_info(data_path, "data", "last_time_token_refreshed")
-        t0 = datetime.strptime(last_time, self.time_format)
+    async def verify_login(self) -> bool:
+        t0 = datetime.strptime(self.last_time, self.time_format)
         t1 = datetime.now()
-        print(t0, t1, (t1-t0).total_seconds())
+        # print(t0, t1, (t1-t0).total_seconds())
         if (t1 - t0).total_seconds() >= 3600:
-            return await self.refresh()
+            return False
+        return True
+
+
+    async def return_token(self) -> str:
+        if not await self.verify_login():
+            self.refresh()
         return self.old_token
 
 
-    async def refresh(self) -> str:
+    async def refresh(self):
         '''gets brand new token ready to use'''
         response = post(url = "https://accounts.spotify.com/api/token",
                         data = {"grant_type":"refresh_token",
@@ -84,20 +102,16 @@ class Login:
 
         # if access_token is in the response, the new token will be returned
         if "access_token" in response.json():
-            print("Token refreshed")
-            task = asyncio.create_task(self.hour_timer())
-            Settings().set_info(data_path, "data", "last_time_token_refreshed", datetime.now())
-            return response.json()["access_token"]
-        
-        # error
-        print(response.json(), response.reason, response.status_code)
-        sys.exit(1)
+            token = response.json()["access_token"]
+            print(f"Token refreshed: {token}")
+            Settings().set_info_in_data("last_time_token_refreshed", datetime.now())
+            Settings().set_info_in_secrets("old_token", token)
+            self.last_time = datetime.now()
+            self.old_token = token
 
-    
-    async def hour_timer(self):
-        await asyncio.sleep(3600)
-        token = await self.refresh()
-        await MySpotify(token, operating_system).main()
+        else:
+            print("I could not refresh the token. Exiting.")
+            sys.exit(1)
 
 
 
@@ -105,29 +119,29 @@ class Settings:
     def __init__(self):
         self.secrets_config = RawConfigParser()
         self.data_config = RawConfigParser()
+        self.secrets_config.read(secrets_path)
+        self.data_config.read(data_path)
 
 
-    # gets a value from secrets.ini or data.ini
-    def get_info(self, path:str, section:str, info:str) -> str:
-        if path == data_path:
-            self.data_config.read(path)
-            return self.data_config.get(section, info).strip('"')
-        elif path == secrets_path:
-            self.secrets_config.read(path)
-            return self.secrets_config.get(section, info).strip('"')
+    # gets a value from secrets.ini
+    def get_info_from_secrets(self, info:str) -> str:
+        return self.secrets_config.get("secrets", info).strip('"')
 
+    # gets a value from  data.ini
+    def get_info_from_data(self, info:str) -> str:
+        return self.data_config.get("data", info).strip('"')
 
-    # sets a value in secrets.ini or data.ini
-    def set_info(self, path:str, section:str, info:str, value):
-        with open(path, "w") as file:
-            if path == data_path:
-                self.data_config.read(path)
-                self.data_config.set(section, info, str(value))
-                self.data_config.write(file)
-            elif path == secrets_path:
-                self.secrets_config.read(path)
-                self.secrets_config.set(section, info, str(value))
-                self.secrets_config.write(file)
+    # sets a value in secrets.ini
+    def set_info_in_secrets(self,info:str, value):
+        with open(secrets_path, "w") as file:
+            self.secrets_config.set("secrets", info, str(value))
+            self.secrets_config.write(file)
+
+    # sets a value in data.ini
+    def set_info_in_data(self, info:str, value):
+        with open(data_path, "w") as file:
+            self.data_config.set("data", info, str(value))
+            self.data_config.write(file)
 
 
 
@@ -153,10 +167,7 @@ async def start():
     # checks if current running OS is supported
     verify_operating_system()
     # gets a valid token
-    base_64 = Settings().get_info(secrets_path, "secrets", "base_64")
-    refresh_token = Settings().get_info(secrets_path, "secrets", "refresh_token")
-    old_token = Settings().get_info(secrets_path, "secrets", "old_token")
-    token = await Login(base_64, refresh_token, old_token).verify_login()
+    token = await Login().return_token()
     # starts spotify.py script
     await MySpotify(token, operating_system).main()
 
@@ -166,6 +177,8 @@ if __name__ == "__main__":
     try:
         loop.run_until_complete(start())
     except asyncio.exceptions.TimeoutError:
+        loop.stop()
+    except KeyboardInterrupt:
         loop.stop()
     except Exception as error:
         loop.stop()
